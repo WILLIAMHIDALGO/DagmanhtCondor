bbb248698dba8e10c71430d9c1f711889eaa6e8e
WILLIAMHIDALGO <32102996+WILLIAMHIDALGO@users.noreply.github.com>
2017-10-26 19:56:19 -0500

    Add files via upload

---
+++ aleatorio.c
@@ -0,0 +1,22 @@
+﻿
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+
+ 
+int main()
+{
+    double num;
+    srand(time(NULL));
+    num = 1 + rand() % (91 - 1);
+    FILE *fp;
+    if((fp = fopen("/vagrant/random.txt", "wb")) == NULL) {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    fprintf(fp, "%lf", num);
+    fclose(fp);
+	 
+    return 0;
+}
